53b782070f65f61e85d25f4bf62102855023f424
salidamaharjan <salidamaharjan@outlook.com>
2024-08-22 17:41:12 -0500

    Update CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,10 @@ import Card from "./components/card.jsx";
 
 function App() {
   return (
+    <div className="flex justify-center pt-20">
     <div className="font-bold text-green-700">
-      <Card buttonTitle="Login" title="Know Your Hour"></Card>
+      <Card title="Know Your Hour"></Card>
+    </div>
     </div>
   );
 }
--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -1,7 +1,7 @@
 import Button from "./button";
 function Card({title}) {
   return <div className="w-full max-w-xs">
-  <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+  <form className="bg-white shadow-md border rounded px-8 pt-6 pb-8 mb-4">
     <div>{title}</div>
     <div className="mb-4">
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
