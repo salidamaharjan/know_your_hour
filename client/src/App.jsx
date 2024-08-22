1488d5b3310ef47fde8800b15255ec656cc1e763
salidamaharjan <salidamaharjan@outlook.com>
2024-08-22 17:25:24 -0500

    Create Card component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
+import Card from "./components/card.jsx";
 
 function App() {
-
   return (
     <div className="font-bold text-green-700">
-     Know Your Hour
+      Know Your Hour
+      <Card buttonTitle="Login"></Card>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
