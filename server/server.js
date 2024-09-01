4fb7cc6e6f176a26425fccbb328abc63c91693dd
salidamaharjan <salidamaharjan@outlook.com>
2024-08-31 22:20:18 -0500

    Install cors

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import sequelize from "./config/connection.js";
 import {loginRoute, signupRoute, viewAllHoursRoute} from "./api/index.js";
+import cors from "cors";
 
 const PORT = 3001;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api", loginRoute, signupRoute, viewAllHoursRoute);
 
 sequelize.sync({ force: false }).then(() => {
