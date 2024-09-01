f3147f7573feb50363d9ce17944ff1e08df9040c
salidamaharjan <salidamaharjan@outlook.com>
2024-09-01 12:50:19 -0500

    code refactor

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -46,7 +46,7 @@ function LoginPage() {
                 Password
             </Label>
             <Input
-                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
                 value={password}
                 type="password"
